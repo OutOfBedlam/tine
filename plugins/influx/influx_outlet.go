00e454011d0048449378e498f4f14a9a04cd4615
Kwon, Yeong Eon <happylogin@gmail.com>
2024-08-24 19:38:54 +0900

    influx line protocol outlet

---
+++ plugins/influx/influx_outlet.go
@@ -0,0 +1,217 @@
+package influx
+
+import (
+	"bytes"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/OutOfBedlam/tine/engine"
+	ilp "github.com/influxdata/line-protocol"
+)
+
+func init() {
+	engine.RegisterOutlet(&engine.OutletReg{
+		Name:    "influx",
+		Factory: InfluxOutlet,
+	})
+}
+
+func InfluxOutlet(ctx *engine.Context) engine.Outlet {
+	return &influxOutlet{ctx: ctx}
+}
+
+type influxOutlet struct {
+	sync.RWMutex
+	ctx            *engine.Context
+	enc            *ilp.Encoder
+	out            io.WriteCloser
+	outBytes       *bytes.Buffer
+	outClient      *httpWriter
+	dbName         string
+	tags           map[string]string
+	tagsFromRecord []string
+	totalWritten   uint64
+}
+
+var _ = engine.Outlet((*influxOutlet)(nil))
+
+func (io *influxOutlet) Open() error {
+	conf := io.ctx.Config()
+
+	io.dbName = conf.GetString("db", "metrics")
+
+	io.tags = make(map[string]string)
+	for _, cfg := range conf.GetConfigSlice("tags", nil) {
+		name := cfg.GetString("name", "")
+		value := cfg.GetString("value", "")
+		if name == "" {
+			continue
+		}
+		if value == "" {
+			io.tagsFromRecord = append(io.tagsFromRecord, name)
+		} else {
+			io.tags[name] = value
+		}
+	}
+
+	path := conf.GetString("path", "")
+	timeout := conf.GetDuration("timeout", 3*time.Second)
+	debug := conf.GetBool("debug", false)
+	switch path {
+	case "":
+		io.out = engine.NopCloser(io.ctx.Writer())
+	case "-":
+		io.out = engine.NopCloser(os.Stdout)
+	default:
+		if strings.HasPrefix(path, "http://") || strings.HasPrefix(path, "https://") {
+			transport := &http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+			}
+			client := &http.Client{
+				Transport: transport,
+				Timeout:   timeout,
+			}
+			hw := &httpWriter{
+				client: client,
+				addr:   path,
+			}
+			hw.debug = debug
+			hw.log = func(lvl slog.Level, status int, body string, size int) {
+				io.ctx.Log(lvl, "outlets.influx request", "status", status, "body", body, "sent", size)
+			}
+			io.outClient = hw
+			io.outBytes = &bytes.Buffer{}
+			io.out = engine.NopCloser(io.outBytes)
+		} else {
+			if f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644); err != nil {
+				return err
+			} else {
+				io.out = f
+			}
+		}
+	}
+	io.enc = ilp.NewEncoder(io.out)
+	return nil
+}
+
+func (io *influxOutlet) Close() error {
+	if io.out != nil {
+		io.out.Close()
+	}
+	if io.outClient != nil {
+		io.outClient.client.CloseIdleConnections()
+	}
+	return nil
+}
+
+func (io *influxOutlet) Handle(recs []engine.Record) error {
+	io.Lock()
+	defer io.Unlock()
+	for _, rec := range recs {
+		m, _ := io.rec2Metric(rec)
+		n, err := io.enc.Encode(m)
+		if err != nil {
+			return err
+		}
+		atomic.AddUint64(&io.totalWritten, uint64(n))
+	}
+	if io.outBytes != nil && io.outClient != nil {
+		io.outClient.Write(io.outBytes.Bytes())
+		io.outBytes.Reset()
+	}
+	return nil
+}
+
+func (io *influxOutlet) rec2Metric(rec engine.Record) (ilp.Metric, error) {
+	tags := make(map[string]string) // Initialize the tags map
+	for k, v := range io.tags {
+		if v != "" { // do not add empty strings
+			tags[k] = v
+		}
+	}
+	for _, k := range io.tagsFromRecord {
+		if v := rec.Tags().Get(k); v != nil {
+			if s, ok := v.String(); ok {
+				if s != "" { // do not add empty strings
+					tags[k] = s
+				}
+			}
+		}
+	}
+
+	fields := make(map[string]interface{}) // Initialize the fields map
+	for _, f := range rec.Fields() {
+		switch f.Type() {
+		case engine.INT:
+			if v, ok := f.Value.Int64(); ok {
+				fields[f.Name] = v
+			}
+		case engine.UINT:
+			if v, ok := f.Value.Uint64(); ok {
+				fields[f.Name] = v
+			}
+		case engine.FLOAT:
+			if v, ok := f.Value.Float64(); ok {
+				fields[f.Name] = v
+			}
+		case engine.STRING:
+			if v, ok := f.Value.String(); ok {
+				if v != "" { // do not add empty strings
+					fields[f.Name] = v
+				}
+			}
+		case engine.BOOL:
+			if v, ok := f.Value.Bool(); ok {
+				fields[f.Name] = v
+			}
+		case engine.TIME:
+			if v, ok := f.Value.Time(); ok {
+				fields[f.Name] = v
+			}
+		}
+	}
+	var tm time.Time
+	if v := rec.Tags().Get(engine.TAG_TIMESTAMP); v != nil {
+		if ts, ok := v.Time(); ok {
+			tm = ts
+		}
+	}
+	if tm.IsZero() {
+		tm = time.Now()
+	}
+	return ilp.New(io.dbName, tags, fields, tm)
+}
+
+type httpWriter struct {
+	client *http.Client
+	addr   string
+	debug  bool
+	log    func(lvl slog.Level, status int, body string, size int)
+}
+
+func (hw *httpWriter) Write(p []byte) (n int, err error) {
+	req, err := http.NewRequest("POST", hw.addr, strings.NewReader(string(p)))
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+	rsp, err := hw.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer rsp.Body.Close()
+	if rsp.StatusCode != http.StatusNoContent {
+		body, _ := io.ReadAll(rsp.Body)
+		hw.log(slog.LevelError, rsp.StatusCode, string(body), len(p))
+	} else if hw.debug {
+		body, _ := io.ReadAll(rsp.Body)
+		hw.log(slog.LevelDebug, rsp.StatusCode, string(body), len(p))
+	}
+	return len(p), nil
+}
---
+++ plugins/influx/influx_outlet_test.go
@@ -0,0 +1,102 @@
+package influx_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/OutOfBedlam/tine/engine"
+	_ "github.com/OutOfBedlam/tine/plugins/base"
+	_ "github.com/OutOfBedlam/tine/plugins/influx"
+	_ "github.com/OutOfBedlam/tine/plugins/psutil"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type influxdbContainer struct {
+	testcontainers.Container
+	host_port string
+}
+
+func setupInfluxdb(ctx context.Context) (*influxdbContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "influxdb:1.8.10",
+		ExposedPorts: []string{"8086/tcp"},
+		Env: map[string]string{
+			"INFLUXDB_DB":                "metrics",
+			"INFLUXDB_HTTP_AUTH_ENABLED": "false",
+		},
+		WaitingFor: wait.ForLog("Listening on HTTP"),
+	}
+	influxC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	ip, err := influxC.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+	mappedPort, err := influxC.MappedPort(ctx, "8086")
+	if err != nil {
+		return nil, err
+	}
+	host_port := ip + ":" + mappedPort.Port()
+	return &influxdbContainer{Container: influxC, host_port: host_port}, nil
+}
+
+func TestInfluxOutlet(t *testing.T) {
+	ctx := context.Background()
+	influxC, err := setupInfluxdb(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := influxC.Terminate(ctx); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	dsl := fmt.Sprintf(`
+		[log]
+			path = "-"
+			level = "debug"
+			no_color = true
+		[defaults]
+			interval = "3s"
+			count = 3
+		[[inlets.load]]
+			loads = [1,5,15]
+		[[inlets.mem]]
+		[[inlets.host]]
+		[[flows.merge]]
+			wait_limit = "5s"
+			name_infix = "."
+		[[outlets.influx]]
+			## If the database does not exist, create first by running:
+			## curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "metrics"'
+			db = "metrics"
+			path = "http://%s/write?db=metrics"
+			tags = [
+				{name="dc", value="us-east-1"},
+				{name="env", value="prod"},
+				{name="_in"}
+			]
+			## Write timeout, especially for the HTTP request
+			timeout = "3s"
+			## Debug mode for logging the response message from the InfluxDB 
+			debug = true
+	`, influxC.host_port)
+	pipeline, err := engine.New(engine.WithConfig(dsl))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := pipeline.Run(); err != nil {
+		t.Fatal(err)
+	}
+	pipeline.Stop()
+}
